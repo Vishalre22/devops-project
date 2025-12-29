fd105943b6676cc246c33e50cd5bbe94af768f0d
Vishalre22 <154823536+Vishalre22@users.noreply.github.com>
2025-12-29 23:03:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 DevOps CI/CD Project Running on Kubernetes (KIND) Made by Vishal Aramur!"
+    return "🚀 DevOps CI/CD Project Running on Kubernetes (KIND) Made by Vishal!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
