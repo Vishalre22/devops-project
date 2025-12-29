c7a33fdd22ee88698a662eb87e936a4aa1a7a9e7
Ubuntu <ubuntu@ip-172-31-9-1.ap-south-1.compute.internal>
2025-12-29 16:56:18 +0000

    updated the app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 DevOps CI/CD Project Running on Kubernetes (KIND) Made by Vishal!"
+    return "🚀 DevOps CI/CD Project Running on Kubernetes (KIND) Made by Vishal Aramur!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
